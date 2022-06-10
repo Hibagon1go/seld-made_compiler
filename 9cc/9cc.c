30cd1e2c6d18cfce495d38229cada2b41981ca2e
Hibagon1go <hibarayoichiro871@gmail.com>
2022-06-10 21:59:50 +0900

    エラーメッセージを改良

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -26,13 +26,18 @@ struct Token
 
 // 現在着目しているトークン
 Token *token;
+char *user_input;
 
-// エラーを報告するための関数
-// printfと同じ引数を取る
-void error(char *fmt, ...)
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...)
 {
     va_list ap;
     va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
+    fprintf(stderr, "^ ");
     vfprintf(stderr, fmt, ap);
     fprintf(stderr, "\n");
     exit(1);
@@ -53,7 +58,7 @@ bool consume(char op)
 void expect(char op)
 {
     if (token->kind != TK_RESERVED || token->str[0] != op)
-        error("'%c'ではありません", op);
+        error_at(token->str, "'%c'ではありません", op);
     token = token->next;
 }
 
@@ -62,7 +67,7 @@ void expect(char op)
 int expect_number()
 {
     if (token->kind != TK_NUM)
-        error("数ではありません");
+        error_at(token->str, "数ではありません");
     int val = token->val;
     token = token->next;
     return val;
@@ -83,9 +88,10 @@ Token *new_token(TokenKind kind, Token *cur, char *str)
     return tok;
 }
 
-// 入力文字列pをトークナイズしてそれを返す
-Token *tokenize(char *p)
+// 入力文字列をトークナイズしてそれを返す
+Token *tokenize()
 {
+    char *p = user_input;
     Token head;
     head.next = NULL;
     Token *cur = &head;
@@ -111,7 +117,7 @@ Token *tokenize(char *p)
             continue;
         }
 
-        error("トークナイズできません");
+        error_at(p, "トークナイズできません");
     }
 
     new_token(TK_EOF, cur, p);
@@ -126,7 +132,8 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    token = tokenize(argv[1]);
+    user_input = argv[1];
+    token = tokenize();
 
     // アセンブリの前半
     printf(".intel_syntax noprefix\n");
